9d217b867ad130683076a6c2461e46564900cd66
orbitiptv <canodzek@gmail.com>
2026-07-29 10:09:01 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='orbit-install-v1';const ASSETS=['./','index.html','manifest.webmanifest','assets/icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
